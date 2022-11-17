3e94e819ab3e1fcc88061c522adbf5398b439710
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2022-11-17 17:29:47 +0100

    main.py - finished

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ engine = create_engine('sqlite:///database.db', echo=True)
 
 meta = MetaData()
 
+
 def read_table(x):
     result_list = []
     with open(f'{x}.csv') as csv_file:
@@ -14,40 +15,40 @@ def read_table(x):
             result_list.append(dict(row))
     return result_list
 
-clean_measure_table = Table(
-   'clean_measure', meta,
-   Column('id', Integer, primary_key=True),
-   Column('station', String),
-   Column('date', String),
-   Column('precip', String),
-   Column('tobs', Integer)
-)
 
-clean_stations_table = Table(
-   'clean_stations', meta,
-   Column('id', Integer, primary_key=True),
-   Column('station', String),
-   Column('latitude', Float),
-   Column('longitude', Float),
-   Column('elevation', Float),
-   Column('name', String),
-   Column('country', String),
-   Column('state', String)
-)
+if __name__ == '__main__':
+    clean_measure_table = Table(
+        'clean_measure', meta,
+        Column('id', Integer, primary_key=True),
+        Column('station', String),
+        Column('date', String),
+        Column('precip', String),
+        Column('tobs', Integer)
+    )
 
+    clean_stations_table = Table(
+        'clean_stations', meta,
+        Column('id', Integer, primary_key=True),
+        Column('station', String),
+        Column('latitude', Float),
+        Column('longitude', Float),
+        Column('elevation', Float),
+        Column('name', String),
+        Column('country', String),
+        Column('state', String)
+    )
 
-meta.create_all(engine)
-print(engine.table_names())
+    meta.create_all(engine)
+    print(engine.table_names())
 
-conn = engine.connect()
-ins = clean_measure_table.insert()
-clean_measure = read_table('clean_measure')
-conn.execute(ins, clean_measure)
-ins = clean_stations_table.insert()
-clean_stations = read_table('clean_stations')
-conn.execute(ins, clean_stations)
+    conn = engine.connect()
+    ins = clean_measure_table.insert()
+    clean_measure = read_table('clean_measure')
+    conn.execute(ins, clean_measure)
+    ins = clean_stations_table.insert()
+    clean_stations = read_table('clean_stations')
+    conn.execute(ins, clean_stations)
 
-print(conn.execute("SELECT * FROM clean_stations LIMIT 5").fetchall())
+    print(conn.execute("SELECT * FROM clean_stations LIMIT 5").fetchall())
 
-conn.close()
-
+    conn.close()
